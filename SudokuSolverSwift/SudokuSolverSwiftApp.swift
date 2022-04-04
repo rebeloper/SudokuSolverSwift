095d1ee94129fb520a44d59957887fdb0b1368cd
Alex Nagy <therebeloper@gmail.com>
2022-04-04 09:57:56 +0300

    Initial Commit

---
+++ SudokuSolverSwift/SudokuSolverSwiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  SudokuSolverSwiftApp.swift
+//  SudokuSolverSwift
+//
+//  Created by Alex Nagy on 04.04.2022.
+//
+
+import SwiftUI
+
+@main
+struct SudokuSolverSwiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
